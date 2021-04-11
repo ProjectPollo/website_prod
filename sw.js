42b29eeaa7b33edd9fbc2c6d36ecd4778f40f8e5
giacomogiudice <giacomogiudice@bots.github.com>
2021-04-11 09:17:33 +0000

    Deploying from @ 2a76425 🚀

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c26a4429e431aefe5e71.js"
+    "url": "webpack-runtime-d0456d35562df1adea08.js"
   },
   {
     "url": "styles.37fa4fbdcfca01d4bd25.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b102d2428e63b80e0b7993bfa2a3fce1"
+    "revision": "aef6a18a05c80aebfe76d3b4fe91a164"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ad6431e4664bcf916d19.js"
